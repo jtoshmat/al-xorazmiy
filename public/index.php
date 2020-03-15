1c5715bb448e921a0ea0878b6f7ffe8f05d6cf49
Jon Toshmatov <jontoshmatov@yahoo.com>
2020-03-15 12:25:33 -0400

    saving my first code

---
+++ public/index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome to Al-Xorazmiy project!</title>
+</head>
+<body>
+<h1>Welcome!</h1>
+
+
+<img class="photo" src="https://i.ytimg.com/vi/2KYcdZBZXbQ/maxresdefault.jpg">
+
+
+<style>
+    .photo{
+        width: 80%;
+    }
+</style>
+
+</body>
+</html>
